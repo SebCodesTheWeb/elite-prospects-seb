6e4af7e4de415b3c5bcc2449b123d91ab13e12bb
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-06-04 18:30:25 +0200

    fix: removed unneccessary footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -175,22 +175,6 @@ export default function Home() {
                 </tr>
               ))}
             </tbody>
-            <tfoot>
-              {table.getFooterGroups().map((footerGroup) => (
-                <tr key={footerGroup.id}>
-                  {footerGroup.headers.map((header) => (
-                    <th key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.footer,
-                            header.getContext()
-                          )}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </tfoot>
           </table>
         </div>
       </div>
