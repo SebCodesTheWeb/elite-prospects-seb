108b4fac6d8861b286968e076f92cbf9696a7b0d
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-06-06 16:08:17 +0200

    feat: added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import { HStack, Center } from '@chakra-ui/react'
 import { StandingsTable } from '@/components/standings-table/standings-table'
 import { LeagueInfo } from '@/components/league-info/league-info'
+import { Header } from '@/components/header/header'
 
 export default function Home() {
   return (
     <main>
-      <Center w='100%' h='100vh' p='4'>
+      <Header />
+      <Center w='100%' p="8">
         <HStack spacing='8' alignItems='top'>
           <LeagueInfo />
           <StandingsTable />
---
+++ src/components/header/header.tsx
@@ -0,0 +1,27 @@
+import { Flex, HStack, Heading, Icon, Image, Link } from '@chakra-ui/react'
+import { AtSignIcon } from '@chakra-ui/icons'
+
+export const Header = () => {
+  return (
+    <HStack
+      w='100vw'
+      bgColor='blue.800'
+      h='100px'
+      color='white'
+      justify='space-between'
+      p='8'
+    >
+      <Image
+        w='100px'
+        src='https://static.eliteprospects.com/images/ep-logo.svg'
+        alt='elite-prospects-logo'
+      />
+      <Heading size='sm'>
+        <Flex gap='1px'>
+          <Icon as={AtSignIcon} />
+          <Link isExternal={true} href="https://github.com/SebCodesTheWeb">SebCodesTheWeb</Link>
+        </Flex>
+      </Heading>
+    </HStack>
+  )
+}
