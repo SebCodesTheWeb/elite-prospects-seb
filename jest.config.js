d797ab1118b0d9e64ccb469927f637be823006e6
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-06-06 19:57:25 +0200

    feat: set up testing env

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+}
