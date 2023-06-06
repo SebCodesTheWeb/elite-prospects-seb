95a66a379f0ee54dd8b32a475dc614e210ae5e58
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-06-06 13:27:23 +0200

    feat: some cool styling for team data ui

--- src/components/standings-table.tsx
+++ src/components/standings-table.tsx
@@ -9,19 +9,19 @@ import {
   Tr,
   Th,
   Td,
-  Wrap,
   Link,
   TableContainer,
   Grid,
+  Icon,
   GridItem,
-  Flex,
   Box,
   Image,
   Text,
-  Heading,
+  HStack,
   Skeleton,
 } from '@chakra-ui/react'
 import { useStandingsTable } from './use-standings-table'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 export const StandingsTable = () => {
   const { table, expandedRows, toggleRowExpanded, isLoading } =
@@ -106,21 +106,18 @@ export const StandingsTable = () => {
                               boxShadow='md'
                               borderWidth='1px'
                               borderRadius='lg'
+                              bgColor='white'
                             >
                               <Grid templateColumns='repeat(5, 1fr)' gap={6}>
                                 <GridItem colSpan={1}>
                                   <Image
                                     src={teamData.logo}
                                     alt={`${teamData.name} logo`}
-                                    boxSize='100px'
+                                    w='100px'
                                   />
                                 </GridItem>
                                 <GridItem colSpan={2}>
-                                  <Flex
-                                    direction='column'
-                                    justifyContent='space-between'
-                                    height='100%'
-                                  >
+                                  <Stack spacing='1'>
                                     <Text fontSize='xl' fontWeight='bold'>
                                       {teamData.name}
                                     </Text>
@@ -139,41 +136,41 @@ export const StandingsTable = () => {
                                     <Text>
                                       <b>Views:</b> {teamData.views}
                                     </Text>
-                                  </Flex>
+                                  </Stack>
                                 </GridItem>
 
                                 <GridItem colSpan={2}>
-                                  <Flex
-                                    direction='column'
-                                    justifyContent='space-between'
-                                    height='100%'
-                                  >
-                                    <Text>
-                                      <b>Colors:</b> {teamData.colors}
-                                    </Text>
-                                    <Text>
-                                      <b>Arena:</b> {teamData.arena.name} <br />
-                                      <b>Location:</b> {teamData.arena.location}{' '}
-                                      <br />
-                                      <b>Year of Construction:</b>{' '}
-                                      {teamData.arena.yearOfConstruction} <br />
-                                      <b>Capacity:</b> {teamData.arena.capacity}
-                                    </Text>
-                                    <Box>
+                                  <Stack justify='space-between' h='100%'>
+                                    <Stack spacing='1'>
+                                      <Text>
+                                        <b>Colors:</b> {teamData.colors}
+                                      </Text>
+                                      <Text>
+                                        <b>Arena:</b> {teamData.arena.name} in{' '}
+                                        {teamData.arena.location} <br />
+                                      </Text>
+                                    </Stack>
+                                    <HStack spacing='4'>
                                       <Link
                                         isExternal={true}
                                         href={teamData.links.officialWebUrl}
+                                        color='blue.500'
+                                        textDecoration='underline'
                                       >
-                                        Official web page
+                                        Official webpage{' '}
+                                        <Icon as={ExternalLinkIcon} pb='1' />
                                       </Link>
                                       <Link
                                         isExternal={true}
                                         href={teamData.links.eliteprospectsUrl}
+                                        color='blue.500'
+                                        textDecoration='underline'
                                       >
-                                        Elite Prospects
+                                        Elite Prospects page{' '}
+                                        <Icon as={ExternalLinkIcon} pb='1' />
                                       </Link>
-                                    </Box>
-                                  </Flex>
+                                    </HStack>
+                                  </Stack>
                                 </GridItem>
                               </Grid>
                             </Box>
