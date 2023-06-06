d002aa7babcff04623b7b23aa9324857f14dc14a
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-06-06 19:20:16 +0200

    fix: seperated standingstable into teamcard component

--- src/components/standings-table/standings-table.tsx
+++ src/components/standings-table/standings-table.tsx
@@ -7,24 +7,16 @@ import {
   SkeletonText,
   Tbody,
   Thead,
-  Stack,
   Tr,
   Th,
   Td,
-  Link,
   TableContainer,
-  Grid,
-  Icon,
-  GridItem,
   Box,
-  Image,
-  Text,
-  HStack,
   Skeleton,
   Heading,
 } from '@chakra-ui/react'
 import { useStandingsTable } from './use-standings-table'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { TeamCard } from './team-card'
 
 export const StandingsTable = () => {
   const { table, expandedRows, toggleRowExpanded, isLoading } =
@@ -124,80 +116,7 @@ export const StandingsTable = () => {
                             </Box>
                           )}
                           {!teamData.isLoading && (
-                            <Box
-                              padding='4'
-                              boxShadow='md'
-                              borderWidth='1px'
-                              borderRadius='lg'
-                              bgColor='white'
-                              h='150px'
-                            >
-                              <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-                                <GridItem colSpan={1}>
-                                  <Image
-                                    src={teamData.logo}
-                                    alt={`${teamData.name} logo`}
-                                    h='100px'
-                                  />
-                                </GridItem>
-                                <GridItem colSpan={2}>
-                                  <Stack spacing='1'>
-                                    <Text fontSize='xl' fontWeight='bold'>
-                                      {teamData.name}
-                                    </Text>
-                                    <Text>
-                                      <b>Founded:</b> {teamData.founded}
-                                    </Text>
-                                    <Text>
-                                      <b>Country:</b> {teamData.country}
-                                    </Text>
-                                    <Text>
-                                      <b>City:</b> {teamData.city}
-                                    </Text>
-                                    <Text>
-                                      <b>Cap Hit:</b> {teamData.capHit}
-                                    </Text>
-                                    <Text>
-                                      <b>Views:</b> {teamData.views}
-                                    </Text>
-                                  </Stack>
-                                </GridItem>
-
-                                <GridItem colSpan={2}>
-                                  <Stack justify='space-between' h='100%'>
-                                    <Stack spacing='1'>
-                                      <Text>
-                                        <b>Colors:</b> {teamData.colors}
-                                      </Text>
-                                      <Text>
-                                        <b>Arena:</b> {teamData.arena.name} in{' '}
-                                        {teamData.arena.location} <br />
-                                      </Text>
-                                    </Stack>
-                                    <HStack spacing='4'>
-                                      <Link
-                                        isExternal={true}
-                                        href={teamData.links.officialWebUrl}
-                                        color='blue.500'
-                                        textDecoration='underline'
-                                      >
-                                        Official webpage{' '}
-                                        <Icon as={ExternalLinkIcon} pb='1' />
-                                      </Link>
-                                      <Link
-                                        isExternal={true}
-                                        href={teamData.links.eliteprospectsUrl}
-                                        color='blue.500'
-                                        textDecoration='underline'
-                                      >
-                                        Elite Prospects page{' '}
-                                        <Icon as={ExternalLinkIcon} pb='1' />
-                                      </Link>
-                                    </HStack>
-                                  </Stack>
-                                </GridItem>
-                              </Grid>
-                            </Box>
+                            <TeamCard teamData={teamData} />
                           )}
                         </Td>
                       </motion.tr>
